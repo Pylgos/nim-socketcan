004e682a3e4b2c2df03580bff8f8e894303c01ad
Takeyoshi Kikuchi <kikuchi@centurysys.co.jp>
2023-11-30 07:56:53 +0900

    add support for reading from and writing to CAN sockets asynchronously.
    Signed-off-by: Takeyoshi Kikuchi <kikuchi@centurysys.co.jp>

--- socketcan.nim
+++ socketcan.nim
@@ -1,4 +1,4 @@
-import std/[os, nativesockets, posix, strformat, bitops, options, hashes]
+import std/[asyncdispatch, os, nativesockets, posix, strformat, bitops, options, hashes]
 
 
 let
@@ -11,11 +11,11 @@ type
   sockaddr_can {.importc: "struct sockaddr_can", header: "<linux/can.h>"} = object
     can_family {.importc.}: cushort
     can_ifindex {.importc.}: cint
-  
+
   can_frame {.importc: "struct can_frame", header: "<linux/can.h>"} = object
-    can_id{.importc.}: uint32
-    len{.importc.}: uint8
-    data{.importc.}: array[8, uint8]
+    can_id {.importc.}: uint32
+    len {.importc.}: uint8
+    data {.importc.}: array[8, uint8]
 
 const
   IdSlice = 0..28
@@ -26,16 +26,17 @@ const
 proc ioctl(fd: cint, request: culong): cint {.importc, header: "<sys/ioctl.h>", varargs.}
 
 type
-  CANSocketObj = object
+  CANSocketObj[HandleType] = object
     isOpened: bool
-    handle: SocketHandle
-  
-  CANSocket* = ref CANSocketObj
+    handle: HandleType
+
+  CANSocket* = ref CANSocketObj[SocketHandle]
+  AsyncCANSocket* = ref CANSocketObj[AsyncFD]
 
   CANKind* = enum
     Data
     Remote
-  
+
   CANFormat* = enum
     Standard
     Extended
@@ -48,7 +49,7 @@ type
     format*: CANFormat
     len*: int
     data*: array[8, byte]
-  
+
   CANError* = object of IOError
 
   ZeroLengthReadError* = object of IOError
@@ -58,29 +59,52 @@ proc hash*(a: CANId): Hash {.borrow.}
 proc `$`*(a: CANId): string {.borrow.}
 
 when (NimMajor, NimMinor) > (1, 6):
-  proc `=destroy`(self: CANSocketObj) =
+  proc `=destroy`(self: CANSocketObj[SocketHandle]) =
     if self.isOpened:
       self.handle.close()
+  proc `=destroy`(self: CANSocketObj[AsyncFD]) =
+    if self.isOpened:
+      try:
+        unregister(self.handle)
+      except:
+        discard
+      self.handle.SocketHandle.close()
 else:
-  proc `=destroy`(self: var CANSocketObj) =
+  proc `=destroy`(self: var CANSocketObj[SocketHandle]) =
     if self.isOpened:
       self.handle.close()
+  proc `=destroy`(self: var CANSocketObj[AsyncFD]) =
+    if self.isOpened:
+      try:
+        unregister(self.handle)
+      except:
+        discard
+      self.handle.SocketHandle.close()
 
 proc close*(self: CANSocket) =
   if self.isOpened:
     self.handle.close()
     self.isOpened = false
 
-func isOpened*(self: CANSocket): bool =
+proc close*(self: AsyncCANSocket) =
+  if self.isOpened:
+    unregister(self.handle)
+    self.handle.SocketHandle.close()
+    self.isOpened = false
+
+func isOpened*(self: CANSocket | AsyncCanSocket): bool =
   self.isOpened
 
 func getHandle*(self: CANSocket): SocketHandle =
   doAssert self.isOpened
   self.handle
 
-proc createCANSocket*(name: string): CANSocket =
-  new result
-  result.handle = createNativeSocket(PF_CAN, posix.SOCK_RAW, CAN_RAW)
+func getHandle*(self: AsyncCANSocket): SocketHandle =
+  doAssert self.isOpened
+  self.handle.SocketHandle
+
+proc createCANSocketHandle(name: string): SocketHandle =
+  result = createNativeSocket(PF_CAN, posix.SOCK_RAW, CAN_RAW)
   let ifindex = if_nametoindex(name)
   if ifindex == 0:
     raise newException(IOError):
@@ -88,27 +112,38 @@ proc createCANSocket*(name: string): CANSocket =
   var sockaddr: sockaddr_can
   sockaddr.can_family = AF_CAN
   sockaddr.can_ifindex = ifindex
-  let res = result.handle.bindAddr(cast[ptr SockAddr](addr sockaddr), sizeof(sockaddr_can).SockLen)
+  let res = result.bindAddr(cast[ptr SockAddr](addr sockaddr), sizeof(sockaddr_can).SockLen)
   if res < 0:
     raiseOSError(osLastError(), name)
   var val = 1.cint
-  discard ioctl(result.handle.cint, FIONBIO.culong, addr val)
+  discard ioctl(result.cint, FIONBIO.culong, addr val)
+
+proc createCANSocket*(name: string): CANSocket =
+  new result
+  result.handle = createCANSocketHandle(name)
+  result.isOpened = true
+
+proc createAsyncCANSocket*(name: string): AsyncCANSocket =
+  new result
+  let handle = createCANSocketHandle(name)
+  result.handle = AsyncFD(handle)
+  register(result.handle)
   result.isOpened = true
 
 proc parseRawFrame(raw: can_frame): CANFrame =
   if raw.can_id.testBit(ErrMsgFrameFlagPos):
     raise newException(CANError, "something bad happened")
-  
+
   if raw.can_id.testBit(RtrFlagPos):
     result.kind = Remote
   else:
     result.kind = Data
-  
+
   if raw.can_id.testBit(FrameFormatFlagPos):
     result.format = Extended
   else:
     result.format = Standard
-  
+
   result.id = raw.can_id.bitsliced(IdSlice).CANId
   result.len = raw.len.int
   result.data = raw.data
@@ -127,6 +162,27 @@ proc read*(self: CANSocket): Option[CANFrame] =
   else:
     result = some parseRawFrame(raw)
 
+proc read*(self: AsyncCANSocket): Future[Option[CANFrame]] =
+  var retFuture = newFuture[Option[CANFrame]]("socketcan.read")
+
+  proc cb(fd: AsyncFD): bool =
+    result = true
+    var raw: can_frame
+    let ret = read(self.handle.cint, addr raw, sizeof(raw))
+    if ret == -1:
+      let error = osLastError()
+      if error == EAGAIN.OSErrorCode:
+        result = false
+      else:
+        retFuture.fail(newException(OSError, osErrorMsg(error)))
+    elif ret == 0:
+      retFuture.fail(newException(ZeroLengthReadError, "zero length read"))
+    else:
+      retFuture.complete(some(parseRawFrame(raw)))
+
+  addRead(self.handle, cb)
+  return retFuture
+
 proc write*(self: CANSocket, frame: CANFrame): bool =
   var raw: can_frame
 
@@ -142,7 +198,7 @@ proc write*(self: CANSocket, frame: CANFrame): bool =
   doAssert frame.len <= 8
   raw.len = frame.len.uint8
   raw.data = frame.data
-  
+
   let ret = write(self.handle.cint, addr raw, sizeof(raw))
   if ret == -1:
     let error = osLastError()
@@ -152,3 +208,35 @@ proc write*(self: CANSocket, frame: CANFrame): bool =
       raiseOSError(error)
   else:
     return true
+
+proc write*(self: AsyncCANSocket, frame: CANFrame): Future[bool] =
+  var retFuture = newFuture[bool]("socketcan.write")
+
+  proc cb(fd: AsyncFD): bool =
+    result = true
+    var raw: can_frame
+    raw.can_id = frame.id.uint32
+    if frame.kind == Remote:
+      raw.can_id.setBit(RtrFlagPos)
+    if frame.format == Extended:
+      doAssert frame.id.uint32 <= ((1 shl 29) - 1)
+      raw.can_id.setBit(FrameFormatFlagPos)
+    else:
+      doAssert frame.id.uint32 <= ((1 shl 12) - 1)
+
+    doAssert frame.len <= 8
+    raw.len = frame.len.uint8
+    raw.data = frame.data
+
+    let ret = write(self.handle.cint, addr raw, sizeof(raw))
+    if ret == -1:
+      let error = osLastError()
+      if error == EAGAIN.OSErrorCode:
+        return false
+      else:
+        retFuture.fail(newException(OSError, osErrorMsg(error)))
+    else:
+      retFuture.complete(true)
+
+  addWrite(self.handle, cb)
+  return retFuture
--- tests/test1.nim
+++ tests/test1.nim
@@ -1,5 +1,6 @@
+import std/asyncdispatch
 import std/[unittest, options]
-import socketcan
+import ../socketcan
 
 
 test "read write":
@@ -17,3 +18,22 @@ test "read write":
   let frameReceived = can1.read()
   check frameReceived.isSome()
   check frameReceived.get() == frame
+
+test "read write (async)":
+  proc test() {.async.} =
+    let can0 = createAsyncCANSocket("vcan0")
+    let can1 = createAsyncCANSocket("vcan0")
+
+    let frame = CANFrame(
+      id: CANId 1234,
+      format: Extended,
+      data: [1, 2, 3, 4, 5, 6, 7, 8],
+      len: 8
+    )
+
+    check await can0.write(frame)
+    let frameReceived = await can1.read()
+    check frameReceived.isSome()
+    check frameReceived.get() == frame
+
+  waitFor test()
